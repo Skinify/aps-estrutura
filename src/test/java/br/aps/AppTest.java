388b63dbe8047e96586ceb46f6b42e2569e12193
Noelen Giovanna <Noelen.giovanna@conam.com.br>
2020-06-03 00:46:50 -0300

    Alterações e testes concluidos

--- src/test/java/br/aps/AppTest.java
+++ src/test/java/br/aps/AppTest.java
@@ -1,18 +1,11 @@
 package br.aps;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
-    }
+    
 }
